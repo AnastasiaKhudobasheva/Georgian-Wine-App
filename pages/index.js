b5528dcc6319421dbfb917b18c491cce57cf82a0
Anastasia K <anastasia.khudobasheva@gmail.com>
2025-07-24 17:02:57 +0200

    added wine list component

--- pages/index.js
+++ pages/index.js
@@ -13,26 +13,31 @@ import WineCard from "@/components/WineCard";
 //     </div>
 //   );
 // }
+import WineList from "@/components/WineList";
 
 export default function Home() {
-  const testWine = {
-    name: "Ori Marani Mamastan PetNat",
-    winemaker: "Ori Marani",
-    grape: ["Tsitska", "Tsolikauri", "Krakhuna"],
-    region: "Imereti",
-    price: 19.95,
-    year: 2024,
-    technology: ["Pet-Nat"],
-    imageUrl: "https://i.imgur.com/LuNCTMQ.png",
-    description:
-      "A naturally sparkling wine made with indigenous Imeretian grapes. Funky, fresh, and fizzy.",
-    slug: "ori-marani-mamastan-petnat-2024",
-  };
-
-  return (
-    <div style={{ padding: "20px", maxWidth: "500px" }}>
-      <h1>Testing Wine Card:</h1>
-      <WineCard wine={testWine} />
-    </div>
-  );
+  return <WineList />;
 }
+
+// export default function Home() {
+//   const testWine = {
+//     name: "Ori Marani Mamastan PetNat",
+//     winemaker: "Ori Marani",
+//     grape: ["Tsitska", "Tsolikauri", "Krakhuna"],
+//     region: "Imereti",
+//     price: 19.95,
+//     year: 2024,
+//     technology: ["Pet-Nat"],
+//     imageUrl: "https://i.imgur.com/LuNCTMQ.png",
+//     description:
+//       "A naturally sparkling wine made with indigenous Imeretian grapes. Funky, fresh, and fizzy.",
+//     slug: "ori-marani-mamastan-petnat-2024",
+//   };
+
+//   return (
+//     <div style={{ padding: "20px", maxWidth: "500px" }}>
+//       <h1>Testing Wine Card:</h1>
+//       <WineCard wine={testWine} />
+//     </div>
+//   );
+// }
